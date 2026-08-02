62259d611d8a46109e46da8b532d4500697c9189
Ahmed Khaled <92623979+a7medk7aledak@users.noreply.github.com>
2026-08-02 18:50:22 +0300

    Update Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -38,7 +38,7 @@ const Projects = () => {
         </div>
       </motion.div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
         {featuredProjects.map((project, index) => (
           <motion.div 
             key={index} 
